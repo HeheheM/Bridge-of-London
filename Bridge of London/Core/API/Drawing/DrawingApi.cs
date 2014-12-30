20d032af5b1d931e5908f003d411bdc6cebc4276
Justin Perez <chewymoonleaguesharp@gmail.com>
2014-12-30 17:32:44 -0500

    fix

--- Bridge of London/Core/API/Drawing/DrawingApi.cs
+++ Bridge of London/Core/API/Drawing/DrawingApi.cs
@@ -47,8 +47,8 @@ namespace Bridge_of_London.Core.API.Drawing
             UserData.RegisterType<RGBA>();
             script.Globals["RGBA"] = (Func<int, int, int ,int, RGBA>) MakeRGBA;
             script.Globals["DrawText"] = (Action<string, int, float, float, RGBA>) DrawText;
-            script.Globals["DrawLine"] = (Action<float, float, float, float, int, RGBA>) DrawLine;
-            script.Globals["DrawCircle"] = (Action<float, float, float, int, RGBA>)  DrawCircle;
+            script.Globals["DrawLine"] = (Action<float, float, float, float, float, RGBA>) DrawLine;
+            script.Globals["DrawCircle"] = (Action<float, float, float, float, RGBA>)  DrawCircle;
         }
 
         private static RGBA MakeRGBA(int r, int g, int b, int a)
@@ -57,12 +57,12 @@ namespace Bridge_of_London.Core.API.Drawing
         }
 
 
-        private static void DrawCircle(float x, float y, float z, int size, RGBA color)
+        private static void DrawCircle(float x, float y, float z, float size, RGBA color)
         {
             LeagueSharp.Drawing.DrawCircle(new Vector3(x, y, z), size, color.ToSystemColor());
         }
 
-        private static void DrawLine(float x1, float x2, float y1, float y2, int size, RGBA color)
+        private static void DrawLine(float x1, float x2, float y1, float y2, float size, RGBA color)
         {
             LeagueSharp.Drawing.DrawLine(x1, y1, x1, x2, size, color.ToSystemColor());
         }
